eaa795077ec9f53ff55c714d56a810b7ca3db10e
essien <69251012+siendesign@users.noreply.github.com>
2024-08-09 23:24:24 +0000

    new packages installed

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -71,9 +71,9 @@ export const Navigation = () => {
         <div className="hidden lg:block">
           <button
             type="button"
-            className="rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
+            className="rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black" 
           >
-            Button text
+            
           </button>
         </div>
         <div className="lg:hidden">
